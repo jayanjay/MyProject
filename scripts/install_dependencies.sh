788c07848e87ccb2ab3030bffc02e355df234934
jayan <jayanjai1986@gmail.com>
2025-06-22 21:29:33 +0530

    updated install_dependencies.sh file

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# Install Node.js and npm (Amazon Linux 2)
+curl -fsSL https://rpm.nodesource.com/setup_18.x | sudo bash -
+sudo yum install -y nodejs
 cd /home/ec2-user/sample-app
+# Now you can use npm
 npm install
